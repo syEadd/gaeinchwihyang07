c883647110857b12727a60857950dc831c2218c7
syEadd <zec05024@naver.com>
2023-07-26 23:24:25 +0900

    butto  Bear 성공!!💄

--- 2023.07.01/Bear Kingdom.js
+++ 2023.07.01/Bear Kingdom.js
@@ -55,4 +55,49 @@ ggo.addEventListener('click',function(ww){
 })
 
 
+// 이미지 변경
+let box4 = document.querySelector('.hot1:nth-child(5) img');
+//  box4.src = './Bear picture/7단계.png' 
+// 1번 방식 ▲ ▲ ▲
+
+//  let img = document.querySelector('.hot1:nth-child(4) img');
+// let box4a = {
+  //   src : './Bear picture/7단계.png'
+  // }
+  // img.src = box4.src
+  // 2번 방식 ▲ ▲ ▲
+  let box5 = document.querySelector('.hot6 img');
+  console.log(box5)
+  const box8 = document.querySelector('.box10')
+  
+  let box46 = document.querySelector('.hot1:nth-child(5)');
+  box46.addEventListener('cilck',function(a){
+  
+    
+  })
+  box4.addEventListener('click',function(e){
+    // 3번적용  ▽ ▽ ▽
+
+    let text_1 = box5.src = box4.src;
+    
+    // // 4번적용  ▽ ▽ ▽
+
+    // box5.src = box4.src;
+  //   let box11 = {
+  //   src :'./Bear picture/9.png'
+  // }
+  // box5.src = box11.src
+    
+    box8.style.display = 'block';
+    e.preventDefault();
+  })
+
+
+
+
+const box6 = document.querySelector('.box11 button');
+
+box6.addEventListener('click',function(){
+  box8.style.display = 'none';
+})
 
